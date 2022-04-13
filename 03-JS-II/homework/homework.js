481370e681abcfd84500941dfab2c52e9d50f538
cuellogabriel <102547787+cuellogabriel@users.noreply.github.com>
2022-04-13 17:47:25 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,7 +1,7 @@
 // No cambies los nombres de las funciones.
 
 function obtenerMayor(x, y) {
-  // "x" e "y" son números enteros (int).
+  // Hola
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
